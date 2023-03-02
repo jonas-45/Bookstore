67b8e711af269c2884c31c958920dc43a66ef3d2
jonas-45 <jonaswullah@gmail.com>
2023-03-02 18:47:28 +0000

    Create routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Root from "./components/Root";
+import Books from "./routes/Books";
+import Categories from "./routes/Categories";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Root />} >
+        <Route path="books" element={<Books />} />
+        <Route path="categories" element={<Categories />} />
+      </Route>
+    </Routes>
   );
 }
 
