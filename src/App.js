934ecb06b54c2ee3a0eb18ec2ddc793259cb8c4f
jonas-45 <jonaswullah@gmail.com>
2023-03-03 11:01:21 +0000

    Add routes to router configuration

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Root from "./components/Root";
 import Books from "./routes/Books";
 import Categories from "./routes/Categories";
+import NotMatch from "./routes/NotMatch";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Root />} >
-        <Route path="books" element={<Books />} />
+        <Route index element={<Books />} />
         <Route path="categories" element={<Categories />} />
+        <Route path="*" element={<NotMatch />} />
       </Route>
     </Routes>
   );
