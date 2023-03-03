fd32ae7c142db7eb81f8114e5ed60da89a2502d5
jonas-45 <jonaswullah@gmail.com>
2023-03-03 10:59:35 +0000

    Add navigation menu links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
+  const links = [
+    { path: '/', text: 'Books' },
+    { path: 'categories', text: 'Categories' }
+  ]
+
   return (
-    <div>Navbar</div>
+    <nav>
+      <div className='logo'>Bookstore CMS</div>
+      <ul>
+        {links.map((link) => (
+          <li><NavLink className="nav-item" to={link.path}>{link.text}</NavLink> </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
