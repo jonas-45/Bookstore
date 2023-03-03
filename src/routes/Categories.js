e0874e1d4b329a5e0c4ad42a6cf75819728be4a4
jonas-45 <jonaswullah@gmail.com>
2023-03-03 11:00:21 +0000

    Centered categories page content

--- src/routes/Categories.js
+++ src/routes/Categories.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Categories() {
   return (
-    <div>Categories</div>
+    <div className='category-wrapper'>
+      <p>Categories page</p>
+    </div>
   )
 }
