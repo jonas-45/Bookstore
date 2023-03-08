4b9d9bf4a676e08c56eeedf884999c61374d2a59
jonas-45 <jonaswullah@gmail.com>
2023-03-08 11:49:50 +0000

    Remove unsused boos array

--- src/routes/Books.js
+++ src/routes/Books.js
@@ -2,34 +2,18 @@ import React from 'react';
 // import { v4 as uuidv4 } from 'uuid';
 import { useSelector } from 'react-redux';
 import Book from '../components/Book';
-import Form from '../components/Form';
+import AddBook from '../components/AddBook';
 
 export default function Books() {
   const { books } = useSelector((state) => state.books);
-  // const booksArr = [
-  //   {
-  //     id: uuidv4(),
-  //     title: 'The Hunger Games',
-  //     author: 'Suzanne Collins',
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     title: 'Dunes',
-  //     author: 'Frank Herbert',
-  //   },
-  //   {
-  //     id: uuidv4(),
-  //     title: 'Capital in The Twenty First Centurey',
-  //     author: 'Suzanne Collins',
-  //   },
-  // ];
+
   return (
     <div>
       {books.map((book) => (
         <Book key={book.item_id} book={book} />
       ))}
       <hr className="horizontal-line" />
-      <Form />
+      <AddBook />
     </div>
   );
 }
