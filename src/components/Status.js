994b762c8235c3c6f3fec0a774d4d471a65845f2
jonas-45 <jonaswullah@gmail.com>
2023-03-11 12:57:24 +0000

    Add status bar to status component

--- src/components/Status.js
+++ src/components/Status.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-const Status = () => (
+const Status = ({ percent }) => (
   <div className="status-wrapper">
-    <div className="oval">status</div>
+    <div className="oval">
+      <CircularProgressbar value={percent} text={`${percent}%`} />
+    </div>
+    <div className="percent-complete">
+      <span>
+        {percent}
+        %
+      </span>
+      <span className="completed">Completed</span>
+    </div>
   </div>
 );
+
+Status.propTypes = {
+  percent: PropTypes.number.isRequired,
+};
 
 export default Status;
