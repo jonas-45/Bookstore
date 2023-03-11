6faa3cda1c908d4e83e6bde0c3c9373a2258334d
jonas-45 <jonaswullah@gmail.com>
2023-03-11 06:20:35 +0000

    Import status and progress component into book

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { removeBook } from '../redux/books/booksSlice';
+import Status from './Status';
+import Progress from './Progress';
 
 export default function Book({ book }) {
   const dispatch = useDispatch();
@@ -11,16 +13,24 @@ export default function Book({ book }) {
 
   return (
     <div className="book">
-      <span className="category">{category}</span>
-      <h2>{title}</h2>
-      <span className="author">{author}</span>
-      <br />
-      <div className="book-actions-container">
-        <button type="button" className="book-actions">Comments</button>
-        <p className="action-separator">|</p>
-        <button type="button" className="book-actions" onClick={() => dispatch(removeBook(id))}>Remove</button>
-        <p className="action-separator">|</p>
-        <button type="button" className="book-actions">Edit</button>
+      <div className="book-details">
+        <span className="category">{category}</span>
+        <h2>{title}</h2>
+        <span className="author">{author}</span>
+        <br />
+        <div className="book-actions-container">
+          <button type="button" className="book-actions">Comments</button>
+          <p className="action-separator">|</p>
+          <button type="button" className="book-actions" onClick={() => dispatch(removeBook(id))}>Remove</button>
+          <p className="action-separator">|</p>
+          <button type="button" className="book-actions">Edit</button>
+        </div>
+      </div>
+
+      <div className="status-and-progress">
+        <Status />
+        <div>|</div>
+        <Progress />
       </div>
     </div>
   );
